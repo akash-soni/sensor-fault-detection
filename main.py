59595b0ac2edb336e55fdde03448864db48e515b
akash-soni <akash.200287@gmail.com>
2022-11-11 23:56:53 +0530

    db connection obtained

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from sensor.configuration.mongo_db_connection import MongoDBClient
 from sensor.exception import SensorException
 import os, sys
+from sensor.utils.main_utils import read_yaml_file
 from sensor.logger import logging
 from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig
 from sensor.pipeline.training_pipeline import TrainPipeline
 
+env_file_path=os.path.join(os.getcwd(),"env.yaml")
+
+def set_env_variable(env_file_path):
+
+    if os.getenv('MONGO_DB_URL',None) is None:
+        env_config = read_yaml_file(env_file_path)
+        os.environ['MONGO_DB_URL']=env_config['MONGO_DB_URL']
 
 if __name__ == '__main__':
     training_pipeline = TrainPipeline()
