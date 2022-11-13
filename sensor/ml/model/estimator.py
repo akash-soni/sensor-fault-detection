527877f75fd5f182ad48626c9bb4636c0f65f755
akash-soni <akash.200287@gmail.com>
2022-11-13 14:28:03 +0530

    model trainer built

--- sensor/ml/model/estimator.py
+++ sensor/ml/model/estimator.py
@@ -11,3 +11,21 @@ class TargetValueMapping:
         mapping_response = self.to_dict()
         return dict(zip(mapping_response.values(), mapping_response.keys()))
 
+#Write a code to train model and check the accuracy.
+
+class SensorModel:
+
+    def __init__(self,preprocessor,model):
+        try:
+            self.preprocessor = preprocessor
+            self.model = model
+        except Exception as e:
+            raise e
+    
+    def predict(self,x):
+        try:
+            x_transform = self.preprocessor.transform(x)
+            y_hat = self.model.predict(x_transform)
+            return y_hat
+        except Exception as e:
+            raise 
